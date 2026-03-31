0ffe7f63e58e13432559d77786fb0fd8f0d3c682
LibGR <liamboigegrain@gmail.com>
2026-03-31 20:25:36 +0200

    ADD - CICD

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://LibGR.github.io',
+  	base: '/CUBCalifornieDocs',
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'CUBDocumentations',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/LibGR/CUBCalifornieDocs/' }],
 			sidebar: [
 				{
 					label: 'Autre (EPOKA)',
